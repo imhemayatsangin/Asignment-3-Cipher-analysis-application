04c73a0ab58f34f2a1dc2d56d8af022e181a09b0
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-04-25 22:28:02 +0300

    reading from file and replacing rule.

--- analysisapp.cpp
+++ analysisapp.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -22,26 +24,101 @@ bool check_alphabets(char c) {
     return look_table[(unsigned char) c];
 }
 
+string replace_cipher_text(const string& cipher_text, const vector<pair<char,char>>& replacement_rule) {
+    string plain_text = cipher_text;
+    for (char& c : plain_text) {
+        if (check_alphabets(c)) {
+            char uppercase_c = toupper(c);
+            for (auto& pair : replacement_rule) {
+                if (pair.first == uppercase_c) {
+                    char replacement = pair.second;
+                    c = tolower(replacement);
+                    break;
+                }
+            }
+        }
+    }
+    return plain_text;
+}
+
 int main() {
-    string cipher_text;
+      string cipher_text;
     int letter_count[alphabet_size] = {0};
 
-    cout << "Enter the cipher text: ";
-    getline(cin, cipher_text);
+    ifstream file("cipher.txt");
+
+    if (file.is_open()) {
+        getline(file, cipher_text);
+        file.close();
+    } else {
+        cout << "Unable to open file";
+        return 1;
+    }
 
     for (char c : cipher_text) {
         if (check_alphabets(c)) {
             letter_count[toupper(c) - 'A']++;
         }
     }
+cout << "Cipher Text:" << endl;
+cout << cipher_text<< endl;
+    cout << "Analysis:" << endl;
 
-    cout<<"Analysis:"<<endl;
-
+    // Find the highest frequency
+    int highest_frequency = 0;
     for (int i = 0; i < alphabet_size; i++) {
-        char letter = 'A' + i;
-        if (letter_count[i] > 0) {
-            cout << letter << "->" << letter_count[i] << ",";
+        if (letter_count[i] > highest_frequency) {
+            highest_frequency = letter_count[i];
         }
+    }
+
+    // Display letters with the highest frequency first
+    for (int j = highest_frequency; j > 0; j--) {
+        for (int i = 0; i < alphabet_size; i++) {
+            if (letter_count[i] == j) {
+                char letter = 'A' + i;
+                cout << letter << "->" << letter_count[i] << ",";
+            }
+        }
+    }
+    cout << endl;
+
+    cout << "Option:" << endl;
+    cout << "1) Take replace rule" << endl;
+    cout << "2) Exit" << endl;
+
+    int option;
+    cout << "Option> ";
+    cin >> option;
+
+    if (option == 1) {
+        string replace_rule;
+        cout << "Enter replacement rule: ";
+        cin.ignore();
+        getline(cin, replace_rule);
+
+        vector<pair<char,char>> replacement_vec;
+        size_t pos = 0;
+        while (pos < replace_rule.length()) {
+            size_t comma_pos = replace_rule.find(',', pos);
+            if (comma_pos == string::npos) {
+                break;
+            }
+            string pair = replace_rule.substr(pos, comma_pos - pos);
+            size_t colon_pos = pair.find(':');
+            if (colon_pos == string::npos) {
+                cout << "Invalid replace rule format. Please try again." << endl;
+                return 1;
+            }
+            char cipher_char = pair[colon_pos - 1];
+            char plain_char = pair[colon_pos + 1];
+            replacement_vec.push_back(make_pair(toupper(cipher_char), tolower(plain_char)));
+            pos = comma_pos + 1;
+        }
+
+        string plain_text = replace_cipher_text(cipher_text, replacement_vec);
+        cout<<"Plain Text: "<<endl;
+        cout << plain_text << endl;
     }
 
     return 0;
